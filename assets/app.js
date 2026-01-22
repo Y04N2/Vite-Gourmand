43a1f79487319a8167f26a64fb3f7ab99ca65e35
Joncart yoann <m.j.yoann@gmail.com>
2026-01-22 12:32:11 +0100

    ajout bouton hamburger et anime JS

--- assets/app.js
+++ assets/app.js
@@ -1,10 +1,14 @@
 import './stimulus_bootstrap.js';
-/*
- * Welcome to your app's main JavaScript file!
- *
- * This file will be included onto the page via the importmap() Twig function,
- * which should already be in your base.html.twig.
- */
 import './styles/app.css';
 
 console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
+
+document.addEventListener('DOMContentLoaded', () => {
+  const toggle = document.querySelector('.menu-toggle');
+
+  if (!toggle) return;
+
+  toggle.addEventListener('click', () => {
+    toggle.classList.toggle('opened');
+  });
+});

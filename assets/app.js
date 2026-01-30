2bfd75941697c9c05855c5202256393eb0fe6b51
Joncart yoann <m.j.yoann@gmail.com>
2026-01-30 12:46:11 +0100

    probelem footer install webpack encore

--- assets/app.js
+++ assets/app.js
@@ -1,44 +1,25 @@
+import 'bootstrap';
 import './stimulus_bootstrap.js';
-import './styles/app.css';
 
 console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
 
- document.addEventListener('DOMContentLoaded', () => {
-  /*const toggle = document.querySelector('.menu-toggle');*/
-    const menu = document.querrySelector('#navbarMenu');
-
-  if (!toggle) return;
-
-  /*toggle.addEventListener('click', () => {
-    toggle.classList.toggle('opened');*/
-    menu.addEventListener('show.bs.collapse', () => {
-    });
-    menu.addEvenListener('hidden.bs.collapse',() => {
-  });
-});
-
-/*ouverture menu de navigation */
-
+/* ouverture / fermeture menu hamburger */
 const toggle = document.querySelector('.menu-toggle');
 const menu = document.querySelector('#menu');
 
-toggle.addEventListener('click', () => {
-  const isOpen = toggle.classList.toggle('opened');
+if (toggle && menu) {
+    toggle.addEventListener('click', () => {
+        const isOpen = toggle.classList.toggle('opened');
+        menu.classList.toggle('open', isOpen);
+        toggle.setAttribute('aria-expanded', isOpen);
+    });
 
-  menu.classList.toggle('open', isOpen);
-  toggle.setAttribute('aria-expanded', isOpen);
-});
-
-/* fermeture du menu */
-
-document.addEventListener('click', (e) => {
-  if (!e.target.closest('.navbar')) {
-    toggle.classList.remove('opened');
-    menu.classList.remove('open');
-    toggle.setAttribute('aria-expanded', 'false');
-  }
-});
-
-
-
-
+    /* fermeture du menu si on clique en dehors */
+    document.addEventListener('click', (e) => {
+        if (!e.target.closest('.navbar')) {
+            toggle.classList.remove('opened');
+            menu.classList.remove('open');
+            toggle.setAttribute('aria-expanded', 'false');
+        }
+    });
+}

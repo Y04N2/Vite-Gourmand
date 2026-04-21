7152797b798e30e0124e4df1bb19776d91e87cd3
Joncart yoann <m.j.yoann@gmail.com>
2026-04-21 11:17:48 +0200

    désactivation de bootstrap

--- assets/app.js
+++ assets/app.js
@@ -1,26 +1,8 @@
 import './styles/app.css';
-import 'bootstrap';
-import './stimulus_bootstrap.js';
 
-console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
+const btn = document.querySelector('.menu-toggle');
+const menu = document.querySelector('.navdrop');
 
-/* ouverture / fermeture menu hamburger */
-const toggle = document.querySelector('.menu-toggle');
-const menu = document.querySelector('#menu');
-
-if (toggle && menu) {
-    toggle.addEventListener('click', () => {
-        const isOpen = toggle.classList.toggle('opened');
-        menu.classList.toggle('open', isOpen);
-        toggle.setAttribute('aria-expanded', isOpen);
-    });
-
-    /* fermeture du menu si on clique en dehors */
-    document.addEventListener('click', (e) => {
-        if (!e.target.closest('.navbar')) {
-            toggle.classList.remove('opened');
-            menu.classList.remove('open');
-            toggle.setAttribute('aria-expanded', 'false');
-        }
-    });
-}
+btn.addEventListener('click', () => {
+    menu.classList.toggle('open');
+});

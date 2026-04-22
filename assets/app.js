ed8186deaace6fc425be2d426871b150e842a9b7
Joncart yoann <m.j.yoann@gmail.com>
2026-04-22 11:24:34 +0200

    modification CSS et responsive

--- assets/app.js
+++ assets/app.js
@@ -1,8 +1,12 @@
-import './styles/app.css';
 
-const btn = document.querySelector('.menu-toggle');
-const menu = document.querySelector('.navdrop');
 
-btn.addEventListener('click', () => {
-    menu.classList.toggle('open');
+document.addEventListener('DOMContentLoaded', () => {
+    const btn = document.querySelector('.menu-toggle');
+    const menu = document.querySelector('.navdrop');
+
+    if (btn && menu) {
+        btn.addEventListener('click', () => {
+            menu.classList.toggle('open');
+        });
+    }
 });
